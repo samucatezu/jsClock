226714b7b95a00f81aee230480681763a1157620
Samuel Almeida <barbosasamuel24@gmail.com>
2022-01-20 09:32:31 -0300

    fix code

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-//function declaration
 setInterval(displayTime, 1000);
 
-function displayTime(){
-  //variable
-  const timeNow = new Date();
+function displayTime() {
 
+    const timeNow = new Date();
 
-  let hourOfDay = timeNow.getHours();
-  let minutes = timeNow.getMinutes();
-  let seconds = timeNow.getSeconds();
-  let weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-  let today = weekDay[timeNow.getDay()];
-  let months = timeNow.toLocaleString("default", { month: "long" });
-  let year = timeNow.getFullYear();
-  let period = "AM";
+    let hoursOfDay = timeNow.getHours();
+    let minutes = timeNow.getMinutes();
+    let seconds = timeNow.getSeconds();
+    let weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    let today = weekDay[timeNow.getDay()];
+    let months = timeNow.toLocaleString("default", {
+        month: "long"
+    });
+    let year = timeNow.getFullYear();
+    let period = "AM";
 
-  if (hoursOfDay > 12) {
-    hoursOfDay-= 12;
-    period = "PM";
-  }
+    if (hoursOfDay > 12) {
+        hoursOfDay-= 12;
+        period = "PM";
+    }
 
-  if (hoursOfDay === 0) {
-      hoursOfDay = 12;
-      period = "AM";
-  }
+    if (hoursOfDay === 0) {
+        hoursOfDay = 12;
+        period = "AM";
+    }
 
-  hoursOfDay = hoursOfDay < 10 ? "0" + hoursOfDay : hoursOfDay;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
+    hoursOfDay = hoursOfDay < 10 ? "0" + hoursOfDay : hoursOfDay;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  let time = hoursOfDay + ":" + minutes + ":" + seconds + period;
+    let time = hoursOfDay + ":" + minutes + ":" + seconds + period;
 
-  document.getElementById('Clock').innerHTML = time + " " + today + " " + months + " " + year;
+    document.getElementById('Clock').innerHTML = time + " " + today + " " + months + " " + year;
 
 }
 displayTime();
